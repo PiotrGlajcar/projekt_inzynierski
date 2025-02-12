f0bed3d81288ad7db8cb8821cef8a2f731c474d9
PiotrGlajcar <piotrglajcar@gmail.com>
2025-02-12 18:26:53 +0100

    fix: app.jsx refined

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import Home from './components/Home.jsx';
-import HomeStudent from './components/HomeStudent.jsx';
-import About from './components/About.jsx';
-import Contact from './components/Contact.jsx';
-import ToLogin from './components/ToLogin.jsx';
-import Unauthorized from './components/Unauthorized.jsx';
-import LoggedOut from './components/LoggedOut.jsx';
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './components/Home.jsx'
+import HomeStudent from './components/HomeStudent.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import ToLogin from './components/ToLogin.jsx'
+import Unauthorized from './components/Unauthorized.jsx'
+import LoggedOut from './components/LoggedOut.jsx'
 import ManageCourse from "./components/ManageCourse.jsx";
-import ListCourses from './components/ListCourses.jsx';
+import ListCourses from './components/ListCourses.jsx'
 import CreateCourse from "./components/CreateCourse.jsx";
-import CourseRegister from './components/CourseRegister.jsx';
+import CourseRegister from './components/CourseRegister.jsx'
 import RedirectPage from "./components/RedirectPage.jsx";
 import ProtectedRoute from './components/ProtectedRoute.jsx';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MyCourses from './components/MyCourses.jsx';
-import MyCourseDetails from './components/MyCourseDetails.jsx';
-
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import MyCourses from './components/MyCourses.jsx'
+import MyCourseDetails from './components/MyCourseDetails.jsx'
 
 function App() {
 
@@ -35,39 +34,50 @@ function App() {
             {/* Unauthorized Page */}
             <Route path='/unauthorized' element={<Unauthorized />} />
             <Route path="/redirect" element={<RedirectPage />} />
-            <Route path='/home-staff' element={
-              <ProtectedRoute role="teacher">
-                <Home />
-              </ProtectedRoute>}
-            />
+            <Route path='/logged-out' element={<LoggedOut />} />
+            
+            {/* Student Routes */}
             <Route path='/home-student' element={
               <ProtectedRoute role="student">
                 <HomeStudent />
-              </ProtectedRoute> }
-            />
-            <Route path='/unauthorized' element={<Unauthorized />} />
-            <Route path='logged-out' element={<LoggedOut />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
+              </ProtectedRoute> }/>
+
+            <Route path="/my-courses" element={
+              <ProtectedRoute role="student">
+                <MyCourses />
+              </ProtectedRoute> }/>
+
+            <Route path="/my-course/:courseId" element={
+              <ProtectedRoute role="student">
+                <MyCourseDetails />
+              </ProtectedRoute> }/>
+
+            <Route path="/course-register/" element={
+              <ProtectedRoute role="student">
+                <CourseRegister />
+              </ProtectedRoute> }/>
+
+            {/* Teacher Routes */}
+            <Route path='/home-staff' element={
+              <ProtectedRoute role="teacher">
+                <Home />
+              </ProtectedRoute> }/>
+
             <Route path="/create-course" element={
               <ProtectedRoute role="teacher">
                 <CreateCourse />
-              </ProtectedRoute> }
-            />
+              </ProtectedRoute> }/>
+
             <Route path="/manage-course/" element={
               <ProtectedRoute role="teacher">
                 <ListCourses />
-              </ProtectedRoute>}
-            />
+              </ProtectedRoute> }/>
+
             <Route path="/manage-course/:courseId" element={
               <ProtectedRoute role="teacher">
                 <ManageCourse />
-              </ProtectedRoute>}
-            />
-            <Route path="/course-register/" element={<CourseRegister />} />
-            <Route path="/course-register/:courseName" element={<CourseRegister />} />
-            <Route path="/my-courses" element={<MyCourses />} />
-            <Route path="/my-course/:courseId" element={<MyCourseDetails />} />
+              </ProtectedRoute> }/>
+
           </Routes>
           </div>
           <Footer />
