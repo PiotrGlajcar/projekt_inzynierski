97735ebe69be33043c3623432539b0ceefc8e02f
PiotrGlajcar <piotrglajcar@gmail.com>
2025-02-15 01:15:15 +0100

    log: sessions storage logs

--- backend/oauth/views/oauth.py
+++ backend/oauth/views/oauth.py
@@ -89,7 +89,9 @@ def process_oauth_callback(request):
 
         # Log the user in to associate the session with the User object
         login(request, user)
-        request.session.save()
+
+        print(f"Session Key: {request.session.session_key}")
+        print(f"Session Data: {request.session.items()}")
 
         redirect_url = f"{settings.FRONTEND_URL}/redirect"
         return redirect(redirect_url)
